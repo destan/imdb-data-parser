7622614e6ddc8cf11daf3c09c59c075e97ade036
aykutakin <aykutakin90@gmail.com>
2012-12-29 00:58:00 +0200

    Regex improved
    No need extra parsing operations for episode informations.

--- source/parser/moviesParser.py
+++ source/parser/moviesParser.py
@@ -1,14 +1,16 @@
 """
-RegExp: /(.*?) (\(\S{4,}\))( \(.*\))?\s?(\{.+?\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)/gm
-pattern: (.*?) (\(\S{4,}\))( \(.*\))?\s?(\{.+?\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)
+RegExp: /(.*?) (\(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)(\(.+?\))\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)/gm
+pattern: (.*?) (\(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)(\(.+?\))\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)
 flags: gm
 6 capturing groups: 
    group 1: (.*?)                               title
    group 2: (\(\S{4,}\))                        year
-   group 3: ( \(.*\))                           type ex:(TV)
-   group 4: (\{.+?\})                           series info ex: {Ally Abroad (#3.1)}
-   group 5: (\{\{SUSPENDED\}\})                 is suspended?
-   group 6: (.*$)                               year
+   group 3: (\(.+\))                            type ex:(TV)
+   group 4: (\{(.*?)(\(.+?\))\})                series info ex: {Ally Abroad (#3.1)}
+   group 5: (.*?)                               episode name ex: Ally Abroad
+   group 6: (\(.+?\))                           episode number ex: (#3.1)
+   group 7: (\{\{SUSPENDED\}\})                 is suspended?
+   group 8: (.*$)                               year
 
 """
 
@@ -21,21 +23,14 @@ file = open("/home/destan/Desktop/movies.list")
 outputFile = open("/home/destan/Desktop/movies.out", "w")
 counter = 0
 fuckedUpCount = 0
-matcherPattern = "(.*?) (\(\S{4,}\))( \(.*\))?\s?(\{.+?\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)"
-episodeMatcherPattern = "\{.+?(\(.*\))?\}"
+matcherPattern = "(.*?) (\(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)(\(.+?\))\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)"
 
 for line in file:
   matcher = RegExHelper(line)
   isMatch = matcher.match(matcherPattern)
 
   if(isMatch):
-    processedType = matcher.group(3)
-
-    # Check if SUSPENDED is correctly placed
-    if(matcher.group(3) == "{{SUSPENDED}}"):
-        processedType = ""
-
-    outputFile.write(matcher.group(1) + "," + matcher.group(2) + "," + processedType + "," + matcher.group(4) + "," + matcher.group(5) + "," + matcher.group(6) + "\n")
+    outputFile.write(matcher.group(1) + "," + matcher.group(2) + "," + matcher.group(3) + "," + matcher.group(5) + "," + matcher.group(6) + "," + matcher.group(7) + "," + matcher.group(8) + "\n")
   else:
     print "This line is fucked up: " + line
     fuckedUpCount += 1
