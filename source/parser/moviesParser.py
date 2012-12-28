f2bf8ef894cd2d7f2ef2b814534b430c1769a2f2
Destan Sarpkaya <destan@dorukdestan.com>
2012-12-28 22:23:45 +0200

    fixes parsers

--- source/parser/moviesParser.py
+++ source/parser/moviesParser.py
@@ -1,3 +1,17 @@
+"""
+RegExp: /(.*?) (\(\S{4,}\))( \(.*\))?\s?(\{.+?\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)/gm
+pattern: (.*?) (\(\S{4,}\))( \(.*\))?\s?(\{.+?\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)
+flags: gm
+6 capturing groups: 
+   group 1: (.*?)                               title
+   group 2: (\(\S{4,}\))                        year
+   group 3: ( \(.*\))                           type ex:(TV)
+   group 4: (\{.+?\})                           series info ex: {Ally Abroad (#3.1)}
+   group 5: (\{\{SUSPENDED\}\})                 is suspended?
+   group 6: (.*$)                               year
+
+"""
+
 import time
 from ..utils.Utils import *
 
@@ -7,12 +21,21 @@ file = open("/home/destan/Desktop/movies.list")
 outputFile = open("/home/destan/Desktop/movies.out", "w")
 counter = 0
 fuckedUpCount = 0
+matcherPattern = "(.*?) (\(\S{4,}\))( \(.*\))?\s?(\{.+?\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)"
+episodeMatcherPattern = "\{.+?(\(.*\))?\}"
 
 for line in file:
   matcher = RegExHelper(line)
-  isMatch = matcher.match("(.*?) (\(\S{4,}\))( \(.*\))?\s?(\{(\{.*?\})?.*?(\(.*\))?\})?\s*(.*$)")
+  isMatch = matcher.match(matcherPattern)
+
   if(isMatch):
-    outputFile.write(matcher.group(1) + "," + matcher.group(2) + "," + matcher.group(3) + "," + matcher.group(4) + "," + matcher.group(5) + "," + matcher.group(6) + "," + matcher.group(7) + "\n")
+    processedType = matcher.group(3)
+
+    # Check if SUSPENDED is correctly placed
+    if(matcher.group(3) == "{{SUSPENDED}}"):
+        processedType = ""
+
+    outputFile.write(matcher.group(1) + "," + matcher.group(2) + "," + processedType + "," + matcher.group(4) + "," + matcher.group(5) + "," + matcher.group(6) + "\n")
   else:
     print "This line is fucked up: " + line
     fuckedUpCount += 1
--- source/parser/plotParser.py
+++ source/parser/plotParser.py
@@ -8,23 +8,36 @@ outputFile = open("/home/destan/Desktop/plot.out", "w")
 counter = 0
 fuckedUpCount = 0
 
+matcherPattern = "(.+?): (.*)"
+seperator = "\t"
+
 title = ""
 plot = ""
 
 for line in file:
   matcher = RegExHelper(line)
-  isMatch = matcher.match("(.+): (.*)")
+  isMatch = matcher.match(matcherPattern)
+
   if(isMatch):
-  	if(matcher.group(1) == "MV"): #Title
-  		//TODO: reset plot, set title, write previous to file
-  	elif(matcher.group(1) == "MV"): #Descriptive text
-  		//TODO add to plot
-  	else:
-  		//ERROR write to console
-  else:
-  	#just ignore this part, useless
+    if(matcher.group(1) == "MV"): #Title
+
+      if(title != ""):
+        outputFile.write(title + seperator + plot + "\n")
+
+      plot = ""
+      title = matcher.group(2)
+
+    elif(matcher.group(1) == "PL"): #Descriptive text
+      plot += matcher.group(2)
+    elif(matcher.group(1) == "BY"):
+      continue
+    else:
+      print "Unhandled abbreviation: " + matcher.group(1) + " in " + line
+  #else:
+    #just ignore this part, useless lines
   
-  outputFile.write(title + "," + plot + "\n")
+# Covers the last item
+outputFile.write(title + seperator + plot + "\n")
   
 outputFile.flush()
 outputFile.close()
