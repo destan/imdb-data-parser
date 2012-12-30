8671eaeefb4e6dba6ad3bbd18c9cc9d5e79aed9f
Aykut Akin <aykutakin90@gmail.com>
2012-12-30 16:22:44 +0200

    .gz extension handling removed

--- idp/utils/fileopenhandler.py
+++ idp/utils/fileopenhandler.py
@@ -9,11 +9,14 @@ def openfile(inputFileName):
 		return open(inputFileName, "r", encoding='iso-8859-1')
 	print("File cannot be found:", inputFileName)
 
-	inputFileName += ".gz"
-	print("Trying to find file:", inputFileName)
-	if os.path.isfile(inputFileName):
-		print("File found:", inputFileName)
-		return gzip.open(inputFileName, 'rt')
-	print("File cannot be found:", inputFileName)
+#
+#this part removed until python 3.3 becomes available for ubuntu LTS and debian
+#
+#inputFileName += ".gz"
+#print("Trying to find file:", inputFileName)
+#if os.path.isfile(inputFileName):
+#	print("File found:", inputFileName)
+#	return gzip.open(inputFileName, 'rt')
+#print("File cannot be found:", inputFileName)
 
 	raise FileNotFoundError
