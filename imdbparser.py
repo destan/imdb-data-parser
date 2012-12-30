3d58504eb5d26d051f4cb538c27fb102561c8548
Destan Sarpkaya <destan@dorukdestan.com>
2012-12-30 14:09:19 +0200

    uses reflection to iterate over imput list and parse

--- imdbparser.py
+++ imdbparser.py
@@ -3,6 +3,7 @@
 import sys
 import argparse
 from idp.parser.moviesparser import MoviesParser
+from idp import settings
 
 # check python version
 if sys.version_info.major != 3:
@@ -19,6 +20,19 @@ print("mode:", args.mode)
 print("source_dir:", args.source_dir)
 print("destination_dir:", args.destination_dir)
 print("update_lists:", args.update_lists)
+
+
+def get_parser_class_for( itemName ):
+    """
+    Thanks to http://stackoverflow.com/a/452981
+    """
+    kls = "idp.parser." + itemName + "parser." + itemName.title() + "Parser"
+    parts = kls.split('.')
+    module = ".".join(parts[:-1])
+    m = __import__( module )
+    for comp in parts[1:]:
+        m = getattr(m, comp)            
+    return m
 
 if args.update_lists:
     from idp.utils import listdownloader
@@ -40,5 +54,14 @@ preferencesMap = {
     "destinationPath": args.destination_dir
 }
 
-movieParser = MoviesParser(preferencesMap)
-movieParser.startProcessing()
+for item in settings.LISTS:
+    try:
+        ParserClass = get_parser_class_for(item)
+    except Exception:
+        print ("No parser found for: " + item)
+        continue
+    print("Parsing " + item + "...")
+    parser = ParserClass(preferencesMap)
+    parser.startProcessing()
+
+print ("All done, enjoy ;)")
