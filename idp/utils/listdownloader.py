a0db28a7ed6d4d22cb98f1d1adf8decb2974f7d7
Zafer CAKMAK <mzcakmak@gmail.com>
2012-12-30 11:33:09 +0200

    fixed #8 implement downloader
    - app downloads required lists by -u parameter now.

---
+++ idp/utils/listdownloader.py
@@ -0,0 +1,21 @@
+from ..settings import *
+from ftplib import FTP
+
+def download():
+    print("lists will downloaded from server:", INTERFACES_SERVER)
+
+    ftp = FTP(INTERFACES_SERVER)
+    ftp.login()
+    download_count = 0
+    for list in LISTS:
+        try:
+            print("started to download list:" + list)
+            r = ftp.retrbinary('RETR pub/misc/movies/database/'+list+'.list.gz',
+                open(INPUT_DIRECTORY + list + '.list.gz', 'wb').write)
+            print(list + "list downloaded successfully")
+            download_count = download_count+1
+        except:
+            print("ERROR: there is a problem when downloading list " + list)
+
+    print(download_count, " lists are downloaded")
+    ftp.quit()
--- imdbparser.py
+++ imdbparser.py
@@ -2,11 +2,7 @@
 
 import sys
 import argparse
-<<<<<<< Updated upstream
 from idp.parser.moviesparser import MoviesParser
-=======
-from idp.parser import moviesparser
->>>>>>> Stashed changes
 
 # check python version
 if sys.version_info.major != 3:
@@ -16,20 +12,21 @@ parser = argparse.ArgumentParser(description="an IMDB data parser")
 parser.add_argument('-t', '--type', help='type of the output files. default: CSV', choices=['TSV', 'SQL', 'DB'])
 parser.add_argument('-s', '--source_dir', help='source directory of interface lists')
 parser.add_argument('-d', '--destination_dir', help='destination directory for outputs')
+parser.add_argument('-u', '--update_lists', action='store_true', help='downloads lists from server')
 
 args = parser.parse_args()
 print("type:", args.type)
 print("source_dir:", args.source_dir)
 print("destination_dir:", args.destination_dir)
-<<<<<<< Updated upstream
+print("update_lists:", args.update_lists)
+
+if args.update_lists:
+    from idp.utils import listdownloader
+    listdownloader.download()
+
 print("Parsing, please wait. This may take very long time...")
 
 preferencesMap = {"type":args.type.lower(), "destinationDir": args.destination_dir}
 
 movieParser = MoviesParser()
 movieParser.startProcessing(preferencesMap)
-=======
-print(args)
-
-moviesparser.parseLists()
->>>>>>> Stashed changes
